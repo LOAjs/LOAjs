365523f79f5c539e9fc1187b40551fdbcdc766c3
eria1402 <pharkahh1402@gmail.com>
2020-03-13 00:52:51 +0900

    ^a^

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'HelloWorldProject';
-  myvar = [1,2,3,4,5,6,7,8,9];
-  
+  title = "black-dashboard-angular";
 }
